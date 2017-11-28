534c898b79c2a75eff6b27c8d96db3e26d9ec2f6
Fernando Mendes <fernando.devmines@gmail.com>
2017-11-28 13:32:02 -0200

    Update Testing.js

--- js/Testing.js
+++ js/Testing.js
@@ -1,11 +1,18 @@
+
 //Cria comentarios, canais e usuarios falsos.
-//Bom para testes.
+//Praticamente a simulação do servidor.
+//Não gera fotos de perfil, e nem os usúarios que 
+//comentam são os que estão no Pannel de usúarios.
+
+//Bom para testes quando se desenvolve um projeto
+//deste tipo sem internet.
+
 const Testing = (_ => {
 
-    //Channels
-
+    //Channels que iremos ter
+    //durante o teste.
     const TestChannels = [
-        "Ruby",
+        "Assembly
         "Javascript",
         "Lua",
         "Node",
@@ -15,16 +22,21 @@ const Testing = (_ => {
         "ActionScript",
         "Python",
     ]
-
+    
+    //Também gera canais bizarros se precizar.
     function generateBizarrousChannels() {
         u.array(u.randInt(5)).map(e => u.randWords).map(e => Channels.registerChannel(s))
     }
-
-
+    
+    //As fotos utilizadas.
+    //Só tem uma, mas se colocar mais
+    //virão aleatorias.
     const randPictures = [
         './img/profilepix.png'
     ]
-
+    
+    //Gera dados suficientes para um comentário.
+    //Apenas gibberish.
     const createSigleChatPacket = e => ({
         userProfilePic: u.randItemOf(randPictures),
         senderName: u.randWords(10),
@@ -32,26 +44,30 @@ const Testing = (_ => {
         msgBody: u.randWords(500)
     })
 
+    //Gera dados suficientes para varios comentários
+    //em varios canais. 
     const createChatDataPayload = e => {
         const payload = {}
         TestChannels.map(s => payload[s] = u.array(u.randInt(10)).map(e => createSigleChatPacket()))
         return payload
     }
-
+    //Gera dados suficientes para uma certa quantidade 
+    //de comentários em alguns canais. 
     const createSizedDataPayload = e => {
         const payload = {}
         TestChannels.map(s => payload[s] = u.array(u.randInt(10)).map(e => createSigleChatPacket()))
         return payload
     }
 
+    //Espalha gibberish por todos os canais.
     function randomlyGenerateComments() {
         Channels.updateChannels(createChatDataPayload())
     }
 
-    //Users
+    //Usúarios de teste.
     const names = [
         'Cold Meson_06',
-        'FuckerFlicker75',
+        'Flicker75',
         'TheOldMan',
         'Poodiepie',
         'BlueDude62',
@@ -59,20 +75,24 @@ const Testing = (_ => {
         'Wheatley_555'
     ]
 
+    //Cria um novo usuario falso.
     const userFactory = e => ({
         avatar: u.randItemOf(randPictures),
-        name: u.randItemOf(u.randWords(30)),
+        name: u.randWords(30),
         id: u.UUID(),
     })
 
+    //Cria a lista de usuarios de teste.
     const buildArrayOfUsers = e => u.array(names.length).map(e => userFactory())
 
+    //Cria uma lista de usuarios falsos de um tamanho especifico.
     const buildSizedArrayOfUsers = e => u.array(e).map(e => userFactory())
-
+    
+    //Registra todos os usuarios falsos.
     const registerFakeUsers = e => Users.registerUserList(buildArrayOfUsers())
 
+    //Registra uma quantidade aleatoria de usuarios falsos.
     const registerRandAmountOfFakeUsers = e => Users.registerUserList(buildSizedArrayOfUsers(e))
-
 
     const options = {
         channel: {
@@ -91,10 +111,10 @@ const Testing = (_ => {
             maxTimeout: 3000,
         }
     }
-
-
+    
     let spawningChannels = false
 
+    //Cria novos canais com intervalos.
     function spawnNextChannel() {
         if (spawningChannels) {
             const newChannelName = u.randWords(options.channel.wordMaxLenght)
@@ -103,7 +123,8 @@ const Testing = (_ => {
             u.callAfter(spawnNextComment, u.randIntBetw(options.channel.minTimeout, options.channel.maxTimeout))
         }
     }
-
+    
+    //Para ou começa a criar canais.
     function toggleNewChannels() {
         spawningChannels = !spawningChannels
         if (spawningChannels) {
@@ -115,7 +136,8 @@ const Testing = (_ => {
     }
 
     let spawningNewUsers = false
-
+    
+    //Cria novos usuarios.
     function spawnNextUser() {
         if (spawningNewUsers) {
             Users.registerUserList(registerRandAmountOfFakeUsers(u.randIntBetw(options.users.minSpawnAmount, options.users.maxSpawnAmount)))
@@ -123,6 +145,7 @@ const Testing = (_ => {
         }
     }
 
+    //Começa ou para de criar novos usuarios.
     function toggleNewUsers() {
         spawningNewUsers = !spawningNewUsers
         if (spawningNewUsers) {
@@ -133,14 +156,15 @@ const Testing = (_ => {
         }
     }
 
-
+    //Cria novos comentarios com intervalos.
     function spawnNextComment() {
         if (spawningNewComments) {
             Channels.updateChannels(createChatDataPayload())
             u.callAfter(spawnNextComment, u.randIntBetw(options.comments.minTimeout, options.comments.maxTimeout))
         }
     }
-
+    
+    //Para ou começa a criar comentarios.
     function toggleFakeComments() {
         spawningNewComments = !spawningNewComments
         if (spawningNewComments) {
@@ -151,13 +175,14 @@ const Testing = (_ => {
         }
     }
 
+    //Para de gerar dados.
     function stopAll() {
         spawningNewComments = false
         spawningChannels = false
         spawningNewUsers = false
     }
 
-
+    //API publica
     return {
         randomlyGenerateComments,
         registerFakeUsers,
@@ -167,6 +192,8 @@ const Testing = (_ => {
         toggleNewChannels,
         stopAll,
         options,
+        
+        //Faz todos os testes uma vez
         doAllTests() {
             TestChannels.map(s => Channels.registerChannel(s))
             registerFakeUsers()
