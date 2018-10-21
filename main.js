52747dffdcb30b8ab5fbf320bbd2e95306e502db
Fernando Mendes <fernando.mendes@tagview.com.br>
2018-10-21 02:26:47 -0300

    Updates to v2 -- Adds express server

---
+++ main.js
@@ -0,0 +1,169 @@
+const root = 'http://localhost:3000';
+
+const me = {
+  id: null,
+  name: null,
+  avatar: null,
+  previousMessageIndex: 0,
+  messagesSent: [],
+  resetPreviousMessage(){
+    this.previousMessageIndex = 0;
+  },
+  getPreviousMessage(){
+    if(this.previousMessageIndex < this.messagesSent.length)
+      return this.messagesSent[this.previousMessageIndex++];
+  },
+  getNextMessage(){
+    if(this.previousMessageIndex > 0)
+      return this.messagesSent[this.previousMessageIndex--];
+  }
+};
+
+const $$ = selector => {
+  const elements = document.querySelectorAll(selector);
+  if (elements.length === 0) {
+    return false
+  } else if (elements.length === 1) {
+    return elements[0];
+  } else {
+    return new Array.from(elements);
+  }
+};
+
+const formatDate = date => new Date(Date.parse(date)).toString().split(' ')[4];
+
+const formatParrot = parrot => parrot
+  ? "./images/parrot.gif"
+  : "./images/parrot.png";
+
+const json = obj => obj.json();
+
+const createMe = (username, avatar) => fetch(`${root}/me?username=${username}&avatar=${encodeURIComponent(avatar)}`).then(json);
+
+const sendMessage = (author_id, message) => fetch(`${root}/messages?stable=true`, {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ author_id, message }),
+}).then(json);
+
+const unparrot = id => fetch(`${root}/messages/${id}/unparrot`, { method: "PUT" });
+
+const parrot = id => fetch(`${root}/messages/${id}/parrot`, { method: "PUT" });
+
+const updateAll = () => fetch(`${root}/update-all`).then(json);
+
+const recursivePrompt = msg => {
+  const value = prompt(msg);
+  return value ? value : recursivePrompt(msg);
+};
+
+const persist = question => {
+  let value = localStorage.getItem(question, question);
+  if (value) {
+    return value;
+  }
+  value = recursivePrompt(question);
+  localStorage.setItem(question, value);
+  return value;
+};
+
+const parrotCounter = $$('#parrot-counter');
+const form = $$('#form');
+const sendButton = $$('#send-button');
+const profilePicture = $$("#profile-pick");
+const chat = $$("#center");
+const chatContainer = $$("#chat");
+
+const updateScrool = () => {
+  if (chatContainer.scrollTop + 300 + chatContainer.getBoundingClientRect().height > chatContainer.scrollHeight) {
+    chatContainer.scroll({ top: chatContainer.scrollHeight, behavior: 'smooth' });
+  }
+};
+
+const update = async () => {
+  const { parrots, messages } = await updateAll();
+  parrotCounter.innerText = parrots;
+  chat.innerHTML = messages.map(buildMessage).join('\n');
+  updateScrool();
+};
+
+const login = async (username, profile) => {
+  const userName = username || recursivePrompt('Enter your user name') //persist('Enter your user name');
+  const avatarUrl = profile || recursivePrompt('Enter your avatar image url') //persist('Enter your avatar image url');
+  const { avatar, name, id } = await createMe(userName, avatarUrl.length > 10 ? avatarUrl : "https://avatars3.githubusercontent.com/u/9648865?s=460&v=4");
+  me.id = id;
+  me.name = name;
+  me.avatar = avatar;
+  profilePicture.src = avatar;
+  await update();
+  chatContainer.scroll({ top: chatContainer.scrollHeight });
+  setInterval(update, 3000);
+};
+
+const updateParrot = async (messageId, bool) => {
+  if (!JSON.parse(bool)) {
+    await parrot(messageId);
+  } else {
+    await unparrot(messageId);
+  }
+  update();
+};
+
+const buildMessage = ({ id: message_id, content, has_parrot, created_at, user: { avatar, name, id: user_id } }) => `
+<div class="chat-item ${has_parrot ? "has-parrot" : ''}" id="${message_id}" data-user-id="${user_id}">
+  <div class="avatar-container">
+    <img class="avatar" src="${avatar}">
+    </img>
+  </div>
+  <div class="content-container">
+    <div class="metadata">
+      <spam class="user-name">${name}</spam>
+      <div class="bullet"></div>
+      <spam class="send-time">${formatDate(created_at)}</spam>
+      <div class="bullet"></div>
+      <div class="message-parrot" onclick="updateParrot('${message_id}', '${has_parrot}')">
+        <img src="${formatParrot(has_parrot)}" />
+      </div>
+    </div>
+    <div class="message-text">
+      ${content}
+    </div>
+  </div>
+</div>
+`;
+
+const emitMessage = async () => {
+  me.resetPreviousMessage();
+  const messageText = form.value;
+  if (!messageText.trim()) {
+    return;
+  }
+  form.value = "";
+  await sendMessage(me.id, messageText).catch(console.warn);
+  me.messagesSent.unshift(messageText);
+  update();
+};
+
+sendButton.onclick = emitMessage;
+
+const formHasFocus = () => document.activeElement.isSameNode(form);
+
+addEventListener('keydown', ({ shiftKey, keyCode }) => {
+  if (keyCode === 13 && !shiftKey) {
+    emitMessage();
+  }
+  if(keyCode === 38 && formHasFocus()) {
+    const message = me.getPreviousMessage();
+    if(message){
+      form.value = message;
+    }
+  }
+  if(keyCode === 40 && formHasFocus()) {
+    const message = me.getNextMessage();
+    if(message){
+      form.value = message;
+    }
+  }
+});
+
+login();
---
+++ server.js
@@ -0,0 +1,179 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
+});
+
+const server = http.createServer(app);
+
+const defaults = {
+  NAME: 'Guest',
+  AVATAR: 'https://avatars3.githubusercontent.com/u/9648865?s=460&v=4',
+  MESSAGE: 'Private user info',
+  BATCH_SIZE: 200,
+  SERVER_PORT: 3000,
+};
+
+const date = () => new Date().toISOString();
+
+const uuid = (seed = '0123456789abcdef', pattern = '00000000-0000-0000-0000-000000000000') => pattern.replace(/0/g, () => seed[Math.random() * seed.length << 0]);
+
+const sortMessages = (a, b) => Date.parse(a.created_at) > Date.parse(b.created_at);
+
+const User = (name = defaults.NAME, avatar = defaults.AVATAR, id = uuid()) => ({
+  id, name,  avatar,
+});
+
+const Message = (content = defaults.MESSAGE, user = user(), has_parrot = false, created_at = date(), id = uuid()) => ({
+  id, content, has_parrot, created_at, user
+});
+
+const userDatabase = {
+  users: [],
+  createUser(name = defaults.NAME, avatar = defaults.AVATAR){
+    const user = User(name, avatar);
+    this.users.push(user);
+    return user;
+  },
+  getUserById(id){
+    return this.users.find(user => user.id === id);
+  },
+  getAll(){
+    return this.users;
+  },
+};
+
+const shouldUpdateParrot = (message, parrot) => !(message.has_parrot === parrot);
+
+const messageDatabase = {
+  messages: [],
+  parrotCount: 0,
+  createMessage(content = defaults.MESSAGE, user = user()){
+    const message = Message(content, user, false, date());
+    this.messages.push(message);
+    return message;
+  },
+  getMessageById(id){
+    return this.messages.find(message => message.id === id);
+  },
+  getLast(lastCount = 0){
+    if(this.messages.length <= lastCount){
+      return this.messages;
+    }
+    return this.messages.slice(this.messages.length - lastCount, this.messages.length)
+  },
+  setParrot(id, parrot){
+    const message = this.getMessageById(id);
+
+    if(message === undefined){
+      return false;
+    }
+    if(shouldUpdateParrot(message, parrot)){
+      if(parrot) {
+        messageDatabase.parrotCount++;
+      } else {
+        messageDatabase.parrotCount--;
+      }
+      message.has_parrot = parrot;
+    }
+
+    return message;
+  },
+};
+
+const listMessages = () => messageDatabase.getLast(defaults.BATCH_SIZE).concat().sort(sortMessages);
+
+app.get("/me", (req, res) => {
+  const { username, avatar} = req.query;
+  const me = userDatabase.createUser(username, avatar);
+  res.status(200).json(userDatabase.getUserById(me.id));
+});
+
+app.post("/messages", (req, res) => {
+
+  const { author_id, message } = req.body;
+  
+  if(!author_id || !message){
+
+    const properties = [];
+    !message && properties.push("message");
+    !author_id && properties.push("author_id");
+
+    res.status(400).json({
+      type: "missing_property",
+      error: "Um parâmetro requerido não foi informado",
+      properties,
+    });
+
+    return;
+  }
+
+  if(!(req.query.stable === "true") && Math.random() < 0.25){
+    res.status(500).json({
+      type: "internal_error",
+      error: "Houve um erro inesperado",
+    });
+
+    return;
+  }
+
+  const user = userDatabase.getUserById(author_id);
+
+  if(user === undefined) {
+    res.status(404).json({
+      type: "user_not_found",
+      error: "O User informado não existe",
+      user: author_id,
+    });
+    return;
+  }
+
+  res.status(200).json(messageDatabase.createMessage(message, user));
+});
+
+app.put("/messages/:messageId/parrot", (req, res) => {
+  const { messageId } = req.params;
+  const message = messageDatabase.setParrot(messageId, true);
+
+  if(!message){
+    res.status(404).json({
+      type: "message_not_found",
+      error: "A Message informada não existe",
+      message: messageId,
+    });
+    return;
+  }
+
+  res.status(200).json(message);
+});
+
+app.put("/messages/:messageId/unparrot", (req, res) => {
+  const { messageId } = req.params;
+  const message = messageDatabase.setParrot(messageId, false);
+
+  if(!message){
+    res.status(404).json({
+      type: "message_not_found",
+      error: "A Message informada não existe",
+      message: messageId,
+    });
+    return;
+  }
+
+  res.status(200).json(message);
+});
+
+app.get("/update-all", (req, res) => res.status(200).json({
+  messages: listMessages(),
+  parrots: messageDatabase.parrotCount
+}));
+
+server.listen(defaults.SERVER_PORT, () => console.log(`Server running at ${defaults.SERVER_PORT}`));
